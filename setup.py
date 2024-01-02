e44143efa8ee69409c42a27c9048f29932d513dd
malekinho8 <shmeek8@gmail.com>
2024-01-02 17:39:47 -0500

    Updated setup version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fajrGPT",
-    version="1.6",
+    version="1.6.1",
     author="Malek Ibrahim",
     author_email="shmeek8@gmail.com",
     description=("A Python application to assist in waking up for Fajr prayer "
